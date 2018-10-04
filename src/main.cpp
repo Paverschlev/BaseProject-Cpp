ba9fa2ebbee3b6723cdcf7514a1cc3261db5bf6b
pavers <sharekevin001958@gmail.com>
2018-10-04 20:09:01 +0800

    新增函示並測試

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,17 @@ int main() {
     printf("Rule Breaker!!!\n");
     printf("5 + 2 = %d\n", addNumber(5, 2));
     printf("%d\n", getTriangleType(3, 4, 5));
+    printf("%d\n", timesNumber(5, 2));
     return 0;
 }
 #endif // TEST
 
 int addNumber(int a, int b) {
     return a + b;
+}
+
+int timesNumber(int a,int b){
+    return a * b;
 }
 
 int getTriangleType(int a, int b, int c) {
